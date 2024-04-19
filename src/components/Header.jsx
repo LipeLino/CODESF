093731d6b454049aa0cfa32e863c6fa7f417edfe
Mateus Lino <mateusflino@outlook.com>
2024-04-19 18:26:41 -0300

    Nav almost done.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock"
 
 import { brainwave } from "../assets";
 import {navigation} from "../constants";
@@ -14,8 +15,10 @@ const Header = () => {
   const toggleNavigation = () => {
     if (openNavigation) {
         setOpenNavigation(false);
+        enablePageScroll();
     } else {
         setOpenNavigation(true);
+        disablePageScroll();
     }
   };
 
@@ -25,7 +28,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 z-50 w-full bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90
+      className={`fixed top-0 left-0 z-50 w-full border-b border-n-6 lg:bg-n-8/90
     lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8/90' : 'bg-n-8/90 backdrop-blur-sm'}`}
     >
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -38,7 +41,7 @@ const Header = () => {
             >
                 <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                     {navigation.map((item) => (
-                        <a key={item.id} href={item.url}
+                        <a key={item.id} href={item.url} onClick={handleClick}
                         className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 
                         ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold
                         ${item.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/50'
@@ -47,9 +50,8 @@ const Header = () => {
                             {item.title}
                         </a>
                     ))}
-
-                    <HamburgerMenu />
                 </div>
+                <HamburgerMenu />
             </nav>  
             <a href="#newsletter" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block">
                 Newsletter
