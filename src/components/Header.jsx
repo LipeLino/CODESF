596961ddb34534b17e2cbef56d360b049fb57b3c
Lipe Lino <104747472+LipeLino@users.noreply.github.com>
2024-04-23 05:10:13 +0000

    Changing.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,8 +31,8 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-color-ivory lg:bg-color-cream/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-color-cream" : "bg-color-cream/90 backdrop-blur-sm"
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -51,12 +51,12 @@ const Header = () => {
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-color-russianviolet transition-colors hover:text-color-1 ${
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
-                    : "lg:text-color-russianviolet/90"
+                    : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
@@ -71,10 +71,10 @@ const Header = () => {
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          
+          New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          ENTRAR
+          Sign in
         </Button>
 
         <Button
