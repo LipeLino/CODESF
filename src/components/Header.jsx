6fdd3a6dae77bc9b83e7b41726fc22259fb8c4a0
Mateus Lino <mateusflino@outlook.com>
2024-04-25 18:01:39 -0300

    Finally fixed that bug. Thanks, Chrome!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
-import { codesflogo1 } from "../assets";
+import { logocodesf2 } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
@@ -37,7 +37,7 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={codesflogo1} width={190} height={40} alt="Logo CODESF" />
+          <img src={logocodesf2} width={190} height alt="Logo CODESF" />
         </a>
 
         <nav
--- src/constants/index.js
+++ src/constants/index.js
@@ -14,9 +14,6 @@ import {
   framer,
   homeSmile,
   instagram,
-  notification2,
-  notification3,
-  notification4,
   notion,
   photoshop,
   plusSquare,
@@ -34,6 +31,7 @@ import {
   telegram,
   twitter,
   yourlogo,
+  logocodesfblank,
 } from "../assets";
 
 export const navigation = [
@@ -73,7 +71,7 @@ export const navigation = [
 
 export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
 
-export const notificationImages = [notification4, notification3, notification2];
+export const notificationImages = [logocodesfblank];
 
 export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
 
